689e337b8a537bd16e4f44cd47a438c0420447b0
gw11z <89856755+gw11z@users.noreply.github.com>
2021-09-10 06:47:11 -0500

    Added reports

--- Snowflake Connector Python.py
+++ Snowflake Connector Python.py
@@ -44,4 +44,70 @@ display(FlightsDf)
 
 # COMMAND ----------
 
+# Read the View 1 Data
+Report1Df = spark.read \
+  .format("snowflake") \
+  .options(**options) \
+  .option("dbtable", "TOTAL_NUMBER_OF_FLIGHTS_V") \
+  .load()
+
+display(Report1Df)
+
+# COMMAND ----------
+
+# Read the View 2 Data
+Report2Df = spark.read \
+  .format("snowflake") \
+  .options(**options) \
+  .option("dbtable", "ONTIME_PCT_AIRLINE_V") \
+  .load()
+
+display(Report2Df)
+
+# COMMAND ----------
+
+# Read the View 3 Data
+Report3Df = spark.read \
+  .format("snowflake") \
+  .options(**options) \
+  .option("dbtable", "AIRLINE_DELAYS_V") \
+  .load()
+
+display(Report3Df)
+
+# COMMAND ----------
+
+# Read the View 4 Data
+Report4Df = spark.read \
+  .format("snowflake") \
+  .options(**options) \
+  .option("dbtable", "AIRPORT_CANCEL_REASON_V") \
+  .load()
+
+display(Report4Df)
+
+# COMMAND ----------
+
+# Read the View 5 Data
+Report5Df = spark.read \
+  .format("snowflake") \
+  .options(**options) \
+  .option("dbtable", "AIRPORT_DELAY_REASON_V") \
+  .load()
+
+display(Report5Df)
+
+# COMMAND ----------
+
+# Read the View 6 Data
+Report6Df = spark.read \
+  .format("snowflake") \
+  .options(**options) \
+  .option("dbtable", "AIRLINE_UNIQUE_ROUTE_V") \
+  .load()
+
+display(Report6Df)
+
+# COMMAND ----------
+
 
